f985b4ef1ec88ddb31d3e6395f2f36d04bee88ed
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2026-01-30 11:53:33 -0300

    Updating some styles on ProgressDashboard

--- app/(auth)/progress-dashboard/page.tsx
+++ app/(auth)/progress-dashboard/page.tsx
@@ -69,7 +69,7 @@ export default async function ProgressDashboardPage() {
           <h1 className="text-[#111827] text-2xl font-semibold col-span-1">
             Progress
           </h1>
-          <div className="grid grid-cols-4 col-span-4 bg-[#2E3545] text-white text-xs font-semibold border border-[#EFF0F4] rounded-[4px]">
+          <div className="grid grid-cols-4 col-span-4 bg-[#2E3545] text-white text-sm font-semibold border border-[#EFF0F4] rounded-[4px]">
             <div className="border-r border-[#F0F1F5] flex items-center px-4">
               Concept
             </div>
@@ -96,14 +96,14 @@ export default async function ProgressDashboardPage() {
                 <div className="h-full flex flex-col gap-4 px-3.5">
                   <Badge className="bg-[#F4F0FF] text-[#6A35FF]">{`Topic # ${topic.id}`}</Badge>
                   <h3 className="text-sm font-semibold">{topic.name}</h3>
-                  <p className="text-[11px] font-medium text-[#697288]">
+                  <p className="text-xs font-medium text-[#697288]">
                     {topic.description}
                   </p>
                 </div>
               </div>
 
-              <div className="grid grid-cols-4 col-span-4  font-semibold border border-[#E4E5ED] rounded-[10px] mb-[30px]">
-                <div className="border-r border-[#E4E5ED] p-4 h-[172px] grid grid-cols-4 gap-2 content-start">
+              <div className="grid grid-cols-4 col-span-4  font-semibold border border-gray-400 rounded-[10px] mb-[30px]">
+                <div className="border-r border-gray-400 p-4 h-[172px] grid grid-cols-4 gap-2 content-start">
                   {topic.concept_tasks.map((task) => (
                     <TaskItem
                       key={task.id}
@@ -112,7 +112,7 @@ export default async function ProgressDashboardPage() {
                     />
                   ))}
                 </div>
-                <div className="border-r border-[#E4E5ED]  p-4 h-[172px] grid grid-cols-4 gap-8">
+                <div className="border-r border-gray-400  p-4 h-[172px] grid grid-cols-4 gap-8">
                   {topic.excercises_tasks.map((task) => (
                     <TaskItem
                       key={task.id}
@@ -121,7 +121,7 @@ export default async function ProgressDashboardPage() {
                     />
                   ))}
                 </div>
-                <div className="border-r border-[#E4E5ED]  p-4  h-[172px] grid grid-cols-4 gap-8">
+                <div className="border-r border-gray-400  p-4  h-[172px] grid grid-cols-4 gap-8">
                   {topic.startup_tasks.map((task) => (
                     <TaskItem
                       key={task.id}
@@ -274,9 +274,9 @@ const TaskItem = ({
       <ProgressItem
         triggerEl={
           <div
-            className={`size-10 ${completed ? "bg-[#28BF58] text-[#FFFFFF]" : "bg-[#EDF6F0] text-[#8F84AE]"} flex items-center justify-center rounded-[8px] `}
+            className={`size-10 border ${completed ? "bg-[#28BF58] text-[#FFFFFF] border-[#28BF58]" : "bg-[#EDF6F0] text-[#8F84AE] border-gray-400"} flex items-center justify-center rounded-[8px] `}
           >
-            <YoutubeIcon size={18} />
+            <YoutubeIcon size={20} />
           </div>
         }
       >
@@ -287,13 +287,13 @@ const TaskItem = ({
             params="controls=0"
             videoid="TZ43SRdTMs0"
           />
-          <span className="text-[#697288] text-[11px] font-medium mt-4 mb-1 block">
+          <span className="text-[#697288] text-xs font-medium mt-4 mb-1 block">
             YouTube
           </span>
           <h3 className="text-[#111827] text-sm font-semibold mb-3">
             AI allocation in corporate management 2026{" "}
           </h3>
-          <p className="text-[#697288] text-[11px] font-medium mb-1">
+          <p className="text-[#697288] text-xs font-medium mb-1">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
           </p>
@@ -306,25 +306,25 @@ const TaskItem = ({
       <div
         className={`size-10 ${completed ? "bg-[#28BF58] text-[#FFFFFF]" : "bg-[#EDF6F0] text-[#8F84AE]"} flex items-center justify-center rounded-[8px] `}
       >
-        <BookOpenIcon size={18} />
+        <BookOpenIcon size={20} />
       </div>
     );
 
   if (type === "article")
     return (
       <div
-        className={`size-10 ${completed ? "bg-[#28BF58] text-[#FFFFFF]" : "bg-[#EDF6F0] text-[#8F84AE]"} flex items-center justify-center rounded-[8px] `}
+        className={`size-10 border ${completed ? "bg-[#28BF58] text-[#FFFFFF] border-[#28BF58]" : "bg-[#EDF6F0] text-[#8F84AE] border-gray-400"} flex items-center justify-center rounded-[8px] `}
       >
-        <FileTextIcon size={18} />
+        <FileTextIcon size={20} />
       </div>
     );
 
   if (type === "comment")
     return (
       <div
-        className={`size-10 ${completed ? "bg-[#28BF58] text-[#FFFFFF]" : "bg-[#EDF6F0] text-[#8F84AE]"} flex items-center justify-center rounded-[8px] `}
+        className={`size-10 border ${completed ? "bg-[#28BF58] text-[#FFFFFF] border-[#28BF58]" : "bg-[#EDF6F0] text-[#8F84AE] border-gray-400"} flex items-center justify-center rounded-[8px] `}
       >
-        <MessageSquareMoreIcon size={18} />
+        <MessageSquareMoreIcon size={20} />
       </div>
     );
 
